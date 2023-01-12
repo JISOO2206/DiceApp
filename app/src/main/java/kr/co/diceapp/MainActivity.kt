b45ecbdfb5e590435c933acf450880d8a0b878a3
JISOO2206 <900522z@gmail.com>
2023-01-12 23:50:38 +0900

    주사위 굴리기 버튼 클릭 이벤트 생성

--- app/src/main/java/kr/co/diceapp/MainActivity.kt
+++ app/src/main/java/kr/co/diceapp/MainActivity.kt
@@ -2,10 +2,22 @@ package kr.co.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import kr.co.diceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.diceBtn.setOnClickListener {
+            Toast.makeText(this, "주사위 굴림", Toast.LENGTH_SHORT).show()
+        }
     }
 }
